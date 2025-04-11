fcf3b08545f3b67ffa21d7a9a235914c087eeec7
Matin <i.e.matt.tavakoli@gmail.com>
2025-04-11 16:53:56 +0330

    initial commit

--- src/editor/FileManager.java
+++ src/editor/FileManager.java
@@ -1,13 +1,25 @@
 package editor;
 
 import javax.swing.*;
+import java.io.*;
+import java.util.Scanner;
 
 public class FileManager {
     public static void openFile(TextEditor textEditor, JTextArea textArea) {
         JFileChooser fileChooser = new JFileChooser();
+        Scanner scn;
         if (fileChooser.showOpenDialog(textEditor) == JFileChooser.APPROVE_OPTION) {
-            //File file = fileChooser.getSelectedFile();
-            //TODO
+            File file = fileChooser.getSelectedFile();
+            try{
+                scn = new Scanner(file);
+                textArea.setText("");
+                while(scn.hasNextLine()) {
+                    textArea.append(scn.nextLine() + "\n");
+                }
+                textEditor.currentFile = file;
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 
@@ -15,15 +27,26 @@ public class FileManager {
         if (textEditor.currentFile == null){
             JFileChooser fileChooser = new JFileChooser();
             if (fileChooser.showSaveDialog(textEditor) == JFileChooser.APPROVE_OPTION) {
-                //TODO
+                File file = fileChooser.getSelectedFile();
+                try (FileWriter writer = new FileWriter(file)) {
+                    writer.write(textArea.getText());
+                    textEditor.currentFile = file;
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
             }
         }
         else{
-            //TODO
+            try (FileWriter writer = new FileWriter(textEditor.currentFile)) {
+                writer.write(textArea.getText());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 
     public static void newFile(TextEditor textEditor, JTextArea textArea) {
-        //TODO
+        textArea.setText("");
+        textEditor.currentFile = null;
     }
 }
